ec6ea5b18ff2b0eb43bf5b419cc1bb6ce7618d34
Astrid Augusta Yu <astridyu3.14@gmail.com>
2018-07-31 20:46:01 -0700

    Add more weather things

--- src/main/java/io/github/plenglin/goggleapp/weather/WeatherResources.kt
+++ src/main/java/io/github/plenglin/goggleapp/weather/WeatherResources.kt
@@ -2,21 +2,43 @@ package io.github.plenglin.goggleapp.weather
 
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.gson.responseObject
+import org.slf4j.LoggerFactory
 
 object WeatherResources {
+
+    private val log = LoggerFactory.getLogger(javaClass)
 
     const val URL_FORECAST = "http://api.openweathermap.org/data/2.5/forecast"
     const val URL_WEATHER = "http://api.openweathermap.org/data/2.5/weather"
 
     lateinit var apiKey: String
 
-    fun getCurrentWeatherData(lat: Double, lon: Double, cb: (OWMWeatherData?) -> Unit) {
+    fun getCurrentWeatherData(lat: Double, lon: Double, cb: (OWMDataPoint?) -> Unit) {
         Fuel.get(URL_WEATHER, parameters = listOf(
                 "appid" to apiKey,
                 "lon" to lat,
                 "lat" to lon,
                 "units" to "metric"
-        )).responseObject<OWMWeatherData> { _, _, (dat, _) ->
+        )).responseObject<OWMDataPoint> { request, _, (dat, err) ->
+            log.debug("sent {}, received {}", request, dat)
+            if (err != null) {
+                log.error("{}", err)
+            }
+            cb(dat)
+        }
+    }
+
+    fun getForecastData(lat: Double, lon: Double, cb: (OWMForecastData?) -> Unit) {
+        Fuel.get(URL_WEATHER, parameters = listOf(
+                "appid" to apiKey,
+                "lon" to lat,
+                "lat" to lon,
+                "units" to "metric"
+        )).responseObject<OWMForecastData> { request, _, (dat, err) ->
+            log.debug("sent {}, received {}", request, dat)
+            if (err != null) {
+                log.error("{}", err)
+            }
             cb(dat)
         }
     }
--- src/main/java/io/github/plenglin/goggleapp/weather/openweathermap.kt
+++ src/main/java/io/github/plenglin/goggleapp/weather/openweathermap.kt
@@ -1,20 +1,37 @@
 package io.github.plenglin.goggleapp.weather
 
-data class OWMWeatherData(val main: OWMMain,
-                          val weather: List<OWMWeatherSub>,
-                          val wind: OWMWind,
-                          val clouds: OWMClouds,
-                          val city: String)
+data class OWMForecastData(val city: OWMCityInfo,
+                           val cnt: Int,
+                           val list: List<OWMDataPoint>) {
+    init {
+        list.forEach { it.city = this.city.name }
+    }
+}
+
+data class OWMCityInfo(val name: String)
+
+data class OWMDataPoint(val dt: Long,
+                        val main: OWMMain,
+                        val weather: List<OWMWeather>,
+                        val wind: OWMWind,
+                        val clouds: OWMClouds,
+                        var city: String,
+                        val rain: OWMPrecipitation,
+                        val snow: OWMPrecipitation)
 
 data class OWMClouds(val all: Double)
 
-data class OWMWeatherSub(val main: String,
-                         val description: String,
-                         val icon: String)
+data class OWMWeather(val main: String,
+                      val description: String,
+                      val icon: String)
 
 data class OWMWind(val speed: Double,
                    val deg: Double)
 
 data class OWMMain(val temp: Double,
                    val pressure: Double,
-                   val humidity: Double)
+                   val humidity: Double,
+                   val sea_level: Double,
+                   val grnd_level: Double)
+
+data class OWMPrecipitation(val `3h`: Double)
